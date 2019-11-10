0ebe3b13ee92ecc660385553e3835ad4c073ca40
Andrzej Torski <andrew.torski@gmail.com>
2019-11-10 15:54:20 +0100

    Add list thoughts use case

---
+++ lib/domain/ListThoughtsUseCase.dart
@@ -0,0 +1,19 @@
+import 'package:golden_thoughts/domain/Thought.dart';
+import 'package:golden_thoughts/domain/ThoughtRepository.dart';
+
+abstract class ListThoughtsUseCase {
+  List<Thought> thoughts();
+}
+
+class ListThoughts extends ListThoughtsUseCase {
+  
+  ThoughtRepository _repo;
+  
+  ListThoughts(this._repo);
+  
+  @override
+  List<Thought> thoughts() {
+    return _repo.findAll();
+  }
+  
+}
