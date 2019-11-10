53f58908fbeb76a8cc0594ecfe5985961d09bdec
Andrzej Torski <andrew.torski@gmail.com>
2019-11-10 15:14:22 +0100

    Move training layouts to separate file

---
+++ lib/training.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+class TrainingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Golden Thoughts',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: MyHomePage(title: 'Golden Thoughts'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _buildImage(),
+          _buildTitleSection(),
+          _buildButtonSection(),
+          _buildTextSection(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Image _buildImage() => Image.asset('images/lake.jpg',
+      width: 600, height: 240, fit: BoxFit.cover);
+
+  Widget _buildTitleSection() => Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  'Oeschinen Lake Campground',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(color: Colors.grey[500]),
+              )
+            ],
+          ),
+        ),
+        FavouriteWidget(),
+      ],
+    ),
+  );
+
+  Widget _buildButtonSection() {
+    var color = Theme.of(context).primaryColor;
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+                fontSize: 12, fontWeight: FontWeight.w400, color: color),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildTextSection() => Container(
+    padding: const EdgeInsets.all(32),
+    child: Text(
+      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'enjoyed here include rowing, and riding the summer toboggan run.',
+      softWrap: true,
+    ),
+  );
+}
+
+class FavouriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() {
+    // TODO: implement createState
+    return _FavoriteWidgetState();
+  }
+
+}
+
+class _FavoriteWidgetState extends State<FavouriteWidget> {
+  bool _isFavorited = false;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) => Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      Container(
+        padding: EdgeInsets.all(0),
+        child: IconButton(
+          icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+          color: Colors.red[500],
+          onPressed: _toggleFavourite,
+        ),
+      ),
+      SizedBox(
+        width: 18,
+        child: Container(
+          child: Text('$_favoriteCount'),
+        ),
+      )
+    ],
+  );
+
+  void _toggleFavourite() => setState(() {
+    _favoriteCount += _isFavorited ? -1 : 1;
+    _isFavorited = !_isFavorited;
+  });
+}
