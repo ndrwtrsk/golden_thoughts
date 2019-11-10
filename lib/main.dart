4af11494115f06e64e24d4fd72259c5e8d48e012
Andrzej Torski <andrew.torski@gmail.com>
2019-11-10 14:37:45 +0100

    Add image to training layout

--- lib/main.dart
+++ lib/main.dart
@@ -39,8 +39,13 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: [_buildTitleSection()],
+      body: ListView(
+        children: <Widget>[
+          _buildImage(),
+          _buildTitleSection(),
+          _buildButtonSection(),
+          _buildTextSection(),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
@@ -49,6 +54,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+  Image _buildImage() => Image.asset('images/lake.jpg',
+      width: 600, height: 240, fit: BoxFit.cover);
 
   Widget _buildTitleSection() => Container(
         padding: const EdgeInsets.all(32),
@@ -77,6 +85,51 @@ class _MyHomePageState extends State<MyHomePage> {
               color: Colors.red[500],
             )
           ],
+        ),
+      );
+
+  Widget _buildButtonSection() {
+    var color = Theme.of(context).primaryColor;
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+                fontSize: 12, fontWeight: FontWeight.w400, color: color),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildTextSection() => Container(
+        padding: const EdgeInsets.all(32),
+        child: Text(
+          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'enjoyed here include rowing, and riding the summer toboggan run.',
+          softWrap: true,
         ),
       );
 }
