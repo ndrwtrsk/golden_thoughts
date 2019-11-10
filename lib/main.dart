dcc9ab21878e7075739b137c66e862ab4852a950
Andrzej Torski <andrew.torski@gmail.com>
2019-11-10 15:20:34 +0100

    Add new class for root widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:golden_thoughts/training.dart';
 
-void main() => runApp(TrainingApp());
+void main() => runApp(GoldenThoughtsApp());
+
+class GoldenThoughtsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Golden Thoughts',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Golden thoughts'),
+        ),
+        body: Center(
+          child: Text('your thoughts'),
+        ),
+      )
+    );
+  }
+}
